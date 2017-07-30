f0d5c4e685c6e50e842b852cc5ead8561fe379cb
sunny <331942828@qq.com>
2017-07-30 23:43:38 +0800

    xx

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,7 +7,7 @@ class Login extends sczController{
         $this->config->load('weixin');
         $wechatOauth=new Wechat\WechatOauth($this->config->item('weixin'));
         $callback='http://wsp.mzlicai.cn/login/code';
-        $authorizeUrl=$wechatOauth->getOauthRedirect($callback, 'STATE');
+        $authorizeUrl=$wechatOauth->getOauthRedirect($callback, 'STATE','snsapi_userinfo');
         header("Location: $authorizeUrl");
         exit;
     }
@@ -16,7 +16,20 @@ class Login extends sczController{
         print_r($_REQUEST);
         $this->config->load('weixin');
         $wechatOauth=new Wechat\WechatOauth($this->config->item('weixin'));
-        $accessToekn=$wechatOauth->getOauthAccessToken();
-        print_r($accessToekn);
+        $accessToekenInfo=$wechatOauth->getOauthAccessToken();
+        /**
+         * Array
+        (
+            [access_token] => flh_06SWcX-L5y4MPWve2KON6M4TP7IhU-BOKrDXNr_mm_ludqZi1OAGSCYaFyljQuNKFunFiVAYKpMpWT3c5WG075GXp6FmvA5v6lnbiO8
+            [expires_in] => 7200
+            [refresh_token] => -Cd2OK2xEptWZfO9m-STzhGUxcqPSv6FGvhHrbxM9Jo3e4ZBPDqHJSJFAJ2PIfxAwEJn2E52AcSYC6raivHBPNBDdMmc5wDcYsHIpXc5V0E
+            [openid] => o4GSVjm7AeAe-3_8HiXzqqSczA2g
+            [scope] => snsapi_base
+        )
+        */
+        var_dump($accessToekenInfo);
+        //获取用户信息
+        $userInfo=$wechatOauth->getOauthUserInfo($accessToekenInfo['access_token'], $accessToekenInfo['openid']);
+        print_r($userInfo);
     }
 }

801c23b7907826979c8a8d7f4c171a47fe7c799d
sunny <331942828@qq.com>
2017-09-02 20:46:03 +0800

    xx

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,9 +8,13 @@
 
 require_once (APPPATH.'vendor/autoload.php');
 class Home extends SczController{
-    
+        public function __construct() {
+            parent::__construct();
+            $this->load->model('redis/redisSet');
+        }
         public function publicity()
         {
+          
             $this->snsapiWeixin();
             $videoId=intval($this->uri->segment(3));
      
@@ -27,5 +31,17 @@ class Home extends SczController{
             $data['publicityCover']=$video->publicityCover;
             $data['playUrl']='http://'.$_SERVER['HTTP_HOST']."/comment/index/".$videoId;
             $this->load->view('comment/publicity',$data);
+        }
+        public function getToken()
+        {
+             log_message('error', 'Some variable did not contain a value.');
+              $dbUserInfo = $this->db->select('*')->from('user')->where('openId', 'o4GSVjm7AeAe-3_8HiXzqqSczA2g')->get()->result_array();
+              var_dump($dbUserInfo);
+            $userSessionData['a']=111;
+            $userSessionData['b']=111;
+            $this->redisHash->mset(redisKey::USER_INFO_HASH_ID,$userSessionData);
+//            $tToken=$_GET['tToken'];
+           print_r($this->redisSet->smembers('test'));exit;
+
         }
 }
--- application/core/SczController.php
+++ application/core/SczController.php
@@ -8,24 +8,22 @@
 
 class SczController extends CI_Controller {
 
+    static public $tokenExpire = 86400 * 7;
+    public $userInfo;
+
     public function __construct() {
         parent::__construct();
         $this->load->library('session');
         $this->config->load('weixin');
+        $this->load->model('redis/redisHash');
     }
 
     public $result = ['ret' => 0, 'msg' => 'ok'];
 
     public function jsonOutput($isExit = true) {
         $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-
         $allow_origin = array(
-            'http://a.m.com',
-            'http://b.m.com',
-            'http://c.m.com',
-            'http://s.runningdreamer.com',
-            'http://p.runningdreamer.com',
-            'http://a.runningdreamer.com',
+            'http://www.baidu.com'
         );
         if (in_array($origin, $allow_origin)) {
             $this->output->set_header('Access-Control-Allow-Origin:' . $origin);
@@ -41,29 +39,84 @@ class SczController extends CI_Controller {
      * 静默授权
      */
     public function snsapiWeixin() {
-        if ($this->session->has_userdata('userInfo')) {
-            return;
+        log_message('error', 'session Id is:'.session_id());
+        log_message('error', 'session Id is:'.session_id());
+        $userId = $this->redisHash->get(redisKey::USER_SESSION_ID_HASH, session_id());
+        if ($userId) {
+            log_message('error', '从redis中查找到了userId');
+            $this->userInfo = $this->redisHash->all(redisKey::USER_INFO_HASH_ID . $userId);
+//            var_dump($this->userInfo);exit;
+            log_message('error', print_r($this->userInfo));
+            if (time()-$this->userInfo['tokenExpire'] > self::$tokenExpire) {
+                log_message('error', 'redis的用户信息'.print_r($this->userInfo,TRUE));
+                return;
+            }
         }
         $videoId = intval($this->uri->segment(3));
         $wechatOauth = new Wechat\WechatOauth($this->config->item('weixin'));
         //code
         //静默授权的回调地址，获取到code
         if (isset($_GET['code']) && $_GET['state'] = 'base') {
+            log_message('error', "静默授权开始");
             $accessToekenInfo = $wechatOauth->getOauthAccessToken();
             //获取用户信息
-            $userInfo = $wechatOauth->getOauthUserInfo($accessToekenInfo['access_token'], $accessToekenInfo['openid']);
+            $weixinUserInfo = $wechatOauth->getOauthUserInfo($accessToekenInfo['access_token'], $accessToekenInfo['openid']);
+            log_message('error', print_r($weixinUserInfo,TRUE));
             //静默授权获取用户信息失败 采用主动授权
-            if ($userInfo == FALSE) {
-                $authorizeUrl = $wechatOauth->getOauthRedirect($this->config->item('authRedirectUrl','weixin') . '/' . $videoId, 'base', 'userInfo');
+            if ($weixinUserInfo == FALSE) {
+                log_message('error', '静默授权没有获取到微信的基本信息，改为主动授权');
+                $authorizeUrl = $wechatOauth->getOauthRedirect($this->config->item('authRedirectUrl', 'weixin') . '/' . $videoId, 'base', 'snsapi_userinfo');
                 header("Location: $authorizeUrl");
                 exit;
+            } else {
+                log_message('error', '静默授权获取到微信的基本信息');
+                $dbUserInfo = $this->db->select('*')->from('user')->where('openId', $accessToekenInfo['openid'])->get()->result_array(); //获取人员信息
+                log_message('error', print_r($dbUserInfo,true));
+                log_message('error', $this->db->last_query());
+                if (!empty($dbUserInfo)) {
+                    $dbUserInfo=$dbUserInfo[0];
+                    log_message('error', "从数据库获取到用户信息");
+                    $userInfo = array(
+                        'openId' => $dbUserInfo['openid'],
+                        'nickName' => $dbUserInfo['nickname'],
+                        'sex' => $dbUserInfo['sex'],
+                        'province' => $dbUserInfo['province'],
+                        'city' => $dbUserInfo['city'],
+                        'headimgurl' => $dbUserInfo['headimgurl'],
+                        'unionid' => $dbUserInfo['unionid'],
+                        'userId' => $dbUserInfo['id']
+                    );
+                    log_message('error', print_r($userInfo,TRUE));
+                    $userId = $dbUserInfo['id'];
+                } else {
+                    $weixinUserInfo = $wechatOauth->getOauthUserInfo($accessToekenInfo['access_token'], $accessToekenInfo['openid']);
+                    $userInfo = array(
+                        'openId' => $weixinUserInfo['openid'],
+                        'nickName' => $weixinUserInfo['nickname'],
+                        'sex' => $weixinUserInfo['sex'],
+                        'province' => $weixinUserInfo['province'],
+                        'city' => $weixinUserInfo['city'],
+                        'headimgurl' => $weixinUserInfo['headimgurl'],
+                        'unionid' => isset($weixinUserInfo['unionid']) ? $weixinUserInfo['unionid'] : '',
+                    );
+                    $this->db->insert('user', $userInfo);
+                    $userId = $this->db->insert_id();
+                    $userInfo['userId'] = $userId;
+                }
+                $userInfo['token'] = md5($userId . time() . rand(1, 1000));
+                $userInfo['tokenExpire'] = self::$tokenExpire;
+                log_message('error', '开始插入redis，userId is'.$userId);
+                log_message('error', '开始插入redis，userInfo is '.print_r($userInfo,TRUE));
+                $this->redisHash->mset(redisKey::USER_INFO_HASH_ID . $userId, $userInfo);
+                $this->redisHash->set(redisKey::USER_SESSION_ID_HASH, session_id(), $userId);
             }
         }//主动授权获取用户信息
         else if (isset($_GET['code']) && $_GET['state'] = 'userInfo') {
             $accessToekenInfo = $wechatOauth->getOauthAccessToken();
-            $userInfo = $this->db->select('*')->from('user')->where('openId', $accessToekenInfo['openid'])->get()->result(); //获取人员信息
+            $dbUserInfo = $this->db->select('*')->from('user')->where('openId', $accessToekenInfo['openid'])->get()->result_array(); //获取人员信息
             if (!empty($userInfo)) {
-                $userSessionData = array(
+                $dbUserInfo=$dbUserInfo[0];
+                $userInfo = array(
                     'openId' => $userInfo['openid'],
                     'nickName' => $userInfo['nickname'],
                     'sex' => $userInfo['sex'],
@@ -71,11 +124,12 @@ class SczController extends CI_Controller {
                     'city' => $userInfo['city'],
                     'headimgurl' => $userInfo['headimgurl'],
                     'unionid' => isset($userInfo['unionid']) ? $userInfo['unionid'] : '',
-                    'userId'=>$userInfo['id']
+                    'userId' => $userInfo['id']
                 );
+                $userId = $userInfo['id'];
             } else {
                 $weixinUserInfo = $wechatOauth->getOauthUserInfo($accessToekenInfo['access_token'], $accessToekenInfo['openid']);
-                $userSessionData = array(
+                $userInfo = array(
                     'openId' => $weixinUserInfo['openid'],
                     'nickName' => $weixinUserInfo['nickname'],
                     'sex' => $weixinUserInfo['sex'],
@@ -85,25 +139,21 @@ class SczController extends CI_Controller {
                     'unionid' => isset($weixinUserInfo['unionid']) ? $weixinUserInfo['unionid'] : '',
                 );
 
-                $bool = $this->db->insert('user', $userSessionData);
+                $this->db->insert('user', $userInfo);
                 $userId = $this->db->insert_id();
-                $userSessionData['userId']=$userId;
+                $userInfo['userId'] = $userId;
             }
-            $this->session->set_userdata($userSessionData);
+            $userInfo['token'] = md5($userId . time() . rand(1, 1000));
+            $userInfo['tokenExpire'] = self::$tokenExpire;
+            $this->redisHash->mset(redisKey::USER_INFO_HASH_ID . $userId, $userInfo);
+            $this->redisHash->set(redisKey::USER_SESSION_ID_HASH, session_id(), $userId);
         }
         //静默授权，拼接授权地址（设置会调地址），并跳转
         else {
-            $authorizeUrl = $wechatOauth->getOauthRedirect($this->config->item('authRedirectUrl','weixin'). '/' . $videoId, 'base', 'snsapi_base');
+            $authorizeUrl = $wechatOauth->getOauthRedirect($this->config->item('authRedirectUrl', 'weixin') . '/' . $videoId, 'base', 'snsapi_base');
             header("Location: $authorizeUrl");
             exit;
         }
-    }
-
-    /**
-     * 主动授权
-     */
-    public function snsapiUserInfo() {
-        
     }
 
 }

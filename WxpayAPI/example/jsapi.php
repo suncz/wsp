cf0a28b3258c62b95707ae0b76b719386863472a
sunny <331942828@qq.com>
2017-10-10 23:59:07 +0800

    xxx

--- WxpayAPI/example/jsapi.php
+++ WxpayAPI/example/jsapi.php
@@ -18,8 +18,8 @@ function printf_info($data)
 
 //①、获取用户openid
 $tools = new JsApiPay();
-//$openId = $tools->GetOpenid();
-$openId = 'o4GSVjm7AeAe-3_8HiXzqqSczA2g';
+$openId = $tools->GetOpenid();
+//$openId = 'o4GSVjm7AeAe-3_8HiXzqqSczA2g';
 
 //②、统一下单
 $input = new WxPayUnifiedOrder();

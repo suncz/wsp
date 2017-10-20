9d49ec7e858efe9a5367b199caf0524dc0ddb157
sunny <331942828@qq.com>
2017-10-21 00:00:25 +0800

    xx

--- application/controllers/Video.php
+++ application/controllers/Video.php
@@ -153,7 +153,7 @@ class Video extends SczController {
         $videoId=$_GET['videoId'];
         $userId=$_GET['userId'];
       
-        $content= file_get_contents(APPPATH.'../static/invite/invite_1.html');
+        $content= file_get_contents(APPPATH.'../static/invite/invite1.html');
         echo $content ;
         exit;
     }
